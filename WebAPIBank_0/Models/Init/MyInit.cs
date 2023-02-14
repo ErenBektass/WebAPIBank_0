50dbbbbf89bcbe3d4b8820234987b0578bb82f70
Eren Bektaş <erenbektas95@hotmail.com>
2023-02-14 20:09:39 +0300

    Add files via upload

---
+++ WebAPIBank_0/Models/Init/MyInit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank_0.Models.Context;
+using WebAPIBank_0.Models.Entities;
+
+namespace WebAPIBank_0.Models.Init
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            CardInfo ci = new CardInfo();
+            ci.CardUserName = "Eren Bektas";
+            ci.CardNumber = "1111 1111 1111 1995";
+            ci.CardExpiryYear = 2025;
+            ci.CardExpiryMonth = 10;
+            ci.SecurityNumber = "999";
+            ci.Limit = 50000;
+            ci.Balance = 50000;
+            context.Cards.Add(ci);
+            context.SaveChanges();
+        }
+    }
+}
